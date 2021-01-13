2a38fbe230962f5f44f3b21546b7f9e1f8fdfa30
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-01-13 11:07:00 -0500

    Added a DataSource config file

---
+++ src/main/java/com/mcawful/deckbuilder/configs/DataSourceConfig.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.mcawful.deckbuilder.configs;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Michael McAuliffe
+ *
+ */
+@Configuration
+public class DataSourceConfig {
+
+	@Bean
+	public DataSource getDataSource() {
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("org.h2.Driver");
+		dataSourceBuilder.url("jdbc:h2:mem:test");
+		dataSourceBuilder.username("SA");
+		dataSourceBuilder.password("");
+		return dataSourceBuilder.build();
+	}
+}
