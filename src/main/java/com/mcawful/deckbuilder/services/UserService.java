a30c9c21f51784c0b5423ef590d137b7ee3f45b2
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-01-19 16:21:45 -0500

    Added/edited Javadocs

--- src/main/java/com/mcawful/deckbuilder/services/UserService.java
+++ src/main/java/com/mcawful/deckbuilder/services/UserService.java
@@ -22,33 +22,34 @@ public interface UserService {
 	/**
 	 * Method meant to handle a GET operation for a {@link User} object.
 	 * 
-	 * @param id The ID of the {@link User} object.
-	 * @return The related {@link User} object.
-	 * @throws EntityNotFoundException
+	 * @param id The ID of the {@link User} object
+	 * @return The related {@link User} object
+	 * @throws NoSuchElementException When related {@link User} object cannot be
+	 *                                found in the database
 	 */
 	public User getUser(int id) throws NoSuchElementException;
 
 	/**
 	 * Method meant to handle a POST or PUT request for a {@link User} object.
 	 * 
-	 * @param user The {@link User} object to be added or updated.
-	 * @return The added or updated {@link User} object.
-	 * @throws IllegalArgumentException
+	 * @param user The {@link User} object to be added or updated
+	 * @return The added or updated {@link User} object
+	 * @throws IllegalArgumentException When passed a null {@link User} object
 	 */
 	public User createOrUpdateUser(User user) throws IllegalArgumentException;
 
 	/**
 	 * Method for handling a DELETE request for a {@link User} object.
 	 * 
-	 * @param id The ID of the existing {@link User} object.
-	 * @return The deleted {@link User} object.
+	 * @param id The ID of the existing {@link User} object
+	 * @return The deleted {@link User} object
 	 */
 	public void deleteUser(int id);
 
 	/**
 	 * Method for handling a GET request for all {@link User} objects.
 	 * 
-	 * @return A {@link List} of {@link User} objects.
+	 * @return A {@link List} of {@link User} objects
 	 */
 	public List<User> getAllUsers();
 
--- src/main/java/com/mcawful/deckbuilder/services/UserServiceImpl.java
+++ src/main/java/com/mcawful/deckbuilder/services/UserServiceImpl.java
@@ -31,24 +31,54 @@ public class UserServiceImpl implements UserService {
 		this.userRepo = userRepo;
 	}
 
+	/**
+	 * Method to handle a GET operation for a {@link User} object. Calls the
+	 * {@link UserRepo} repository's 'findById' method.
+	 * 
+	 * @param id The ID of the {@link User} object
+	 * @return The related {@link User} object
+	 * @throws NoSuchElementException When related {@link User} object cannot be
+	 *                                found in the database
+	 */
 	@Override
 	public User getUser(int id) throws NoSuchElementException {
 
 		return this.userRepo.findById(id).orElseThrow();
 	}
 
+	/**
+	 * Method to handle a POST or PUT request for a {@link User} object. Calls the
+	 * {@link UserRepo} repository's 'save' method.
+	 * 
+	 * @param user The {@link User} object to be added or updated
+	 * @return The added or updated {@link User} object
+	 * @throws IllegalArgumentException When passed a null {@link User} object
+	 */
 	@Override
 	public User createOrUpdateUser(User user) throws IllegalArgumentException {
 
 		return this.userRepo.save(user);
 	}
 
+	/**
+	 * Method to handle a DELETE request for a {@link User} object. Calls the
+	 * {@link UserRepo} repository's 'deleteById' method.
+	 * 
+	 * @param id The ID of the existing {@link User} object
+	 * @return The deleted {@link User} object
+	 */
 	@Override
 	public void deleteUser(int id) {
 
 		this.userRepo.deleteById(id);
 	}
 
+	/**
+	 * Method to handle a GET request for all {@link User} objects. Calls the
+	 * {@link UserRepo} repository's 'findAll' method.
+	 * 
+	 * @return A {@link List} of {@link User} objects
+	 */
 	@Override
 	public List<User> getAllUsers() {
 
