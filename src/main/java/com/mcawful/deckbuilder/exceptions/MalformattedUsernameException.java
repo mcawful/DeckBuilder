d4b5876799290f102471f0b75c38ddf8b057da7b
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-01-22 17:06:59 -0500

    Added response statuses to custom vailidation exceptions

--- src/main/java/com/mcawful/deckbuilder/exceptions/MalformattedUsernameException.java
+++ src/main/java/com/mcawful/deckbuilder/exceptions/MalformattedUsernameException.java
@@ -3,11 +3,15 @@
  */
 package com.mcawful.deckbuilder.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author Michael McAuliffe
  *
  */
 @SuppressWarnings("serial")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Username is not in a vaild format.")
 public class MalformattedUsernameException extends Exception {
 
 	/**
