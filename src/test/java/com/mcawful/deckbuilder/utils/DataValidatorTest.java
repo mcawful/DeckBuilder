f44fa97cc006e3c4e5f15841f3d9a1bcd456eeb8
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-01-25 11:35:56 -0500

    Added SpringBootTest annotation to the datavalidator test

--- src/test/java/com/mcawful/deckbuilder/utils/DataValidatorTest.java
+++ src/test/java/com/mcawful/deckbuilder/utils/DataValidatorTest.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author Michael McAuliffe
  *
  */
+@SpringBootTest
 class DataValidatorTest {
 
 	private DataValidator dataValidator;
