166432c9b8a10418e5391f4ccfe272f68bb595d0
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-01-27 11:33:47 -0500

    Create GlobalLoggingHandler.java

---
+++ src/main/java/com/mcawful/deckbuilder/aspects/GlobalLoggingHandler.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.mcawful.deckbuilder.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Michael McAuliffe
+ *
+ */
+@Aspect
+@Component
+public class GlobalLoggingHandler {
+
+	
+}
