20158ced492edcbd86e36df90df075de36463d08
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-02-26 23:46:41 -0500

    Altered logging implementation to use built in Spring logging

--- src/main/java/com/mcawful/deckbuilder/aspects/Logging.java
+++ src/main/java/com/mcawful/deckbuilder/aspects/Logging.java
@@ -3,14 +3,14 @@
  */
 package com.mcawful.deckbuilder.aspects;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -21,11 +21,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logging {
 
-	private static Logger repoLog = LogManager.getLogger("Repo");
-	private static Logger serviceLog = LogManager.getLogger("Service");
-	private static Logger controllerLog = LogManager.getLogger("Controller");
-	private static Logger utilLog = LogManager.getLogger("Util");
-
+	private static Logger repoLog = LoggerFactory.getLogger("Repo");
+	private static Logger serviceLog = LoggerFactory.getLogger("Service");
+	private static Logger controllerLog = LoggerFactory.getLogger("Controller");
+	private static Logger utilLog = LoggerFactory.getLogger("Util");
+	
 	@Pointcut("within(com.mcawful.deckbuilder.repos.*)")
 	public void repoClassMethods() {
 		// Method intentionally left empty
@@ -45,7 +45,6 @@ public class Logging {
 	public void utilClassMethods() {
 		// Method intentionally left empty
 	}
-
 	
 	@Before(value = "repoClassMethods() && args(object)")
 	public void logBeforeRepoClassMethods(JoinPoint jp, Object object) {
@@ -55,17 +54,16 @@ public class Logging {
 
 	@AfterReturning(value = "repoClassMethods()", returning = "returnValue")
 	public void logAfterReturningRepoClassMethods(JoinPoint jp, Object returnValue) {
-		
+
 		this.afterReturning(jp, returnValue, repoLog);
 	}
 
 	@AfterThrowing(value = "repoClassMethods()", throwing = "exception")
 	public void logAfterThrowingRepoClassMethods(JoinPoint jp, Throwable exception) {
-		
+
 		this.afterThrowing(jp, exception, repoLog);
 	}
 
-	
 	@Before(value = "serviceClassMethods() && args(object)")
 	public void logBeforeServiceClassMethods(JoinPoint jp, Object object) {
 
@@ -74,17 +72,16 @@ public class Logging {
 
 	@AfterReturning(value = "serviceClassMethods()", returning = "returnValue")
 	public void logAfterReturningServiceClassMethods(JoinPoint jp, Object returnValue) {
-		
+
 		this.afterReturning(jp, returnValue, serviceLog);
 	}
 
 	@AfterThrowing(value = "serviceClassMethods()", throwing = "exception")
 	public void logAfterThrowingServiceClassMethods(JoinPoint jp, Throwable exception) {
-		
+
 		this.afterThrowing(jp, exception, serviceLog);
 	}
-	
-	
+
 	@Before(value = "controllerClassMethods() && args(object)")
 	public void logBeforeControllerClassMethods(JoinPoint jp, Object object) {
 
@@ -93,17 +90,16 @@ public class Logging {
 
 	@AfterReturning(value = "controllerClassMethods()", returning = "returnValue")
 	public void logAfterReturningControllerClassMethods(JoinPoint jp, Object returnValue) {
-		
+
 		this.afterReturning(jp, returnValue, controllerLog);
 	}
 
 	@AfterThrowing(value = "controllerClassMethods()", throwing = "exception")
 	public void logAfterThrowingControllerClassMethods(JoinPoint jp, Throwable exception) {
-		
+
 		this.afterThrowing(jp, exception, controllerLog);
 	}
-	
-	
+
 	@Before(value = "utilClassMethods() && args(object)")
 	public void logBeforeUtilClassMethods(JoinPoint jp, Object object) {
 
@@ -112,16 +108,16 @@ public class Logging {
 
 	@AfterReturning(value = "utilClassMethods()", returning = "returnValue")
 	public void logAfterReturningUtilClassMethods(JoinPoint jp, Object returnValue) {
-		
+
 		this.afterReturning(jp, returnValue, utilLog);
 	}
 
 	@AfterThrowing(value = "utilClassMethods()", throwing = "exception")
 	public void logAfterThrowingUtilClassMethods(JoinPoint jp, Throwable exception) {
-		
+
 		this.afterThrowing(jp, exception, utilLog);
 	}
-	
+
 	/**
 	 * 
 	 * @param jp
@@ -135,7 +131,7 @@ public class Logging {
 		if (objStr.length() > 10)
 			objStr.substring(0, 40);
 
-		log.info(jp.getSignature() + "\nPassed-In: " + objStr);
+		log.info(jp.getSignature() + " - Passed-In: " + objStr);
 	}
 
 	/**
@@ -153,10 +149,10 @@ public class Logging {
 			if (objStr.length() > 10)
 				objStr.substring(0, 40);
 
-			log.info("Returned: " + objStr + " - " + jp.getSignature());
+			log.info(jp.getSignature() + " - Returned: " + objStr);
 
 		} else
-			log.info(jp.getSignature());
+			log.info(jp.getSignature().toString());
 	}
 
 	/**
